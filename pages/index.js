00361abd25db262db1cd5f6e46be5946f618d5df
mukulrajpoot262610 <mukulrajpoot262610@gmail.com>
2022-05-25 21:19:50 +0530

    ADD: login page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,53 @@
+import Image from "next/image";
+import { useState } from "react";
+
 export default function Home() {
+
+  const [userId, setUserId] = useState()
+  const [password, setPassword] = useState()
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <div className="w-full h-screen bg-first_linear bg-cover bg-center bg-no-repeat">
+      <div className="flex p-2">
+        <Image src="/images/sovi_logo.svg" width={100} height={30} alt="" />
+      </div>
+      <div className="w-4/12  mx-auto text-center mt-8">
+        <Image src="/images/user_icon.svg" width={100} height={50} alt="" />
+        <h1 className=" text-4xl text-white">Member Login</h1>
+        <form onSubmit={(e) => loginHandler(e)}>
+          <div className="w-full text-left my-3">
+            <label htmlFor="user" className=" text-white">
+              Username*
+            </label>{" "}
+            <br />
+            <input
+              type="text"
+              required
+              className="w-full p-2 rounded focus:outline-none"
+              value={userId}
+              onChange={(e) => setUserId(e.target.value)}
+            />
+          </div>
+          <div className="w-full text-left mb-3">
+            <label htmlFor="user" className=" text-white">
+              Password*
+            </label>{" "}
+            <br />
+            <input
+              type="password"
+              className="w-full p-2 rounded focus:outline-none"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <button className=" w-full bg-[#FF7948] rounded py-2 text-white mt-4">
+            LOGIN
+          </button>
+          <div className="text-white mt-2 cursor-pointer">
+            Forget password ?
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,19 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        first_linear:
+          "linear-gradient(120.48deg, #3BB1DD 0%, #7273E4 44.37%, #A23DEA 100%)",
+        form_bg: "linear-gradient(360deg, #BAA09F -12.54%, #7C6C6C 99.99%); ",
+      },
+      colors: {
+        "prime-red": "#fa5853",
+      },
+      boxShadow: {
+        shad_prime: "rgba(0, 0, 0, 0.24) 0px 3px 8px",
+      },
+    }
   },
   plugins: [],
 }
