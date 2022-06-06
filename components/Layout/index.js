12ba8aa2ac9e113c2ca9a6b358215a28ee27ffe1
ompgupta <omgupta373@gmail.com>
2022-06-06 16:28:50 +0530

    login_ui_changed

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,28 +1,29 @@
-import React from 'react'
-import { useRouter } from 'next/router'
-import Navbar from './Navbar'
-import Head from 'next/head'
-import { Toaster } from 'react-hot-toast'
+import React from "react";
+import { useRouter } from "next/router";
+import Navbar from "./Navbar";
+import Head from "next/head";
+import { Toaster } from "react-hot-toast";
 
 const Layout = ({ children }) => {
+  const router = useRouter();
+  const path = router.pathname;
 
-    const router = useRouter()
-    const path = router.pathname
+  return (
+    <div>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+          integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
+          crossOrigin="anonymous"
+          referrerpolicy="no-referrer"
+        />
+      </Head>
+      <Toaster />
+      {path !== "/" && <Navbar />}
+      <main>{children}</main>
+    </div>
+  );
+};
 
-    return (
-        <div>
-            <Head>
-                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossOrigin="anonymous" referrerpolicy="no-referrer" />
-            </Head>
-            <Toaster />
-            {
-                path !== '/' && <Navbar />
-            }
-            <main>
-                {children}
-            </main>
-        </div>
-    )
-}
-
-export default Layout
+export default Layout;
